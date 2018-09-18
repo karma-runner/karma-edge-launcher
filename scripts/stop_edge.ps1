644e5cfd106879b108447768bd954278b949f4ae
Tristan VALCKE <valcketristan@gmail.com>
2018-09-18 14:09:26 +0200

    StopEdge: Add alternative exit condition about max loop before exit, and output error in case of reach this limit

--- scripts/stop_edge.ps1
+++ scripts/stop_edge.ps1
@@ -5,6 +5,8 @@
 @description This script will attemp to close gracefully Microsoft Edge tabs then browser itself
 #>
 
+$MAX_TRY = 5000
+$try = 0
 # We need to use force loop because edge process will pop again and again...
 do{
 
@@ -20,4 +22,14 @@ do{
         $MicrosoftEdgeProcess.CloseMainWindow() | out-null
     }
 
-} while( $MicrosoftEdgeCPProcess -or $MicrosoftEdgeProcess )
+    $try++
+
+} while( ( $MicrosoftEdgeCPProcess -or $MicrosoftEdgeProcess ) -and $try -lt $MAX_TRY )
+
+if( $try -eq $MAX_TRY ){
+    Write-Output "Unable to close Microsoft Edge browser and child process correctly."
+    exit 1
+} else {
+    exit 0
+}
+
